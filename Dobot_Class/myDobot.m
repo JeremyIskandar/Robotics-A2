2cecd0d27a34adfd53e91e812d1a97b2501e8bd2
Nathan Rees <nathan.r.rees@student.uts.edu.au>
2022-05-04 10:46:32 +1000

    added updated Dobot_Class and README

--- Dobot_Class/myDobot.m
+++ Dobot_Class/myDobot.m
@@ -3,7 +3,7 @@
 classdef myDobot < handle
     properties
         model;
-        workspace = [-0.5 0.5 -0.5 0.5 0 0.5];  % define the boundaries of the workspace 
+        workspace = [-0.5 0.5 -0.5 0.5 0 0.5];  % define the boundaries of the workspace
     end
 
     methods
@@ -15,21 +15,22 @@ classdef myDobot < handle
         function GetDobot(self)
             pause(0.001);
 
-            L1 = Link('d',0.103,'a',0,     'alpha',pi/2, 'qlim',[-pi/2       pi/2]);
-            L2 = Link('d',0,    'a',0.135, 'alpha',0,    'qlim',[5*pi/180    pi/2]);
-            L3 = Link('d',0,    'a',0.147, 'alpha',0,    'qlim',[-pi/2 10*pi/180]);     % these limits are not quite right based on images of dobot
-            L4 = Link('d',0,    'a',0.0385,'alpha',-pi/2,'qlim',[-pi/2       pi/2]);    % 0.0385 calculated using pythag and known angles/distances
+            L1 = Link('d',0.103,'a',0,     'alpha',pi/2, 'qlim',deg2rad([-135 135]));
+            L2 = Link('d',0,    'a',0.135, 'alpha',0,    'qlim',deg2rad([5    80]));
+            L3 = Link('d',0,    'a',0.147, 'alpha',0,    'qlim',deg2rad([15   170]),'offset',-pi);
+            L4 = Link('d',0,    'a',0.0385,'alpha',-pi/2,'qlim',deg2rad([-90  90])); % 0.0385 calculated using pythag and known angles/distances
+            L5 = Link('d',0,    'a',0,     'alpha',0,    'qlim',deg2rad([-85  85])); % rotation servo is technically 5th joint
 
-            self.model = SerialLink([L1 L2 L3 L4],'name','myDobot');
-            self.model.base = self.model.base * transl(0,0,0.08);  % set base location of robot (adjusted to suit envinronment)
+            self.model = SerialLink([L1 L2 L3 L4 L5],'name','myDobot');
+            self.model.base = self.model.base * transl(0,0,0.1); % set base location of robot (adjusted to suit envinronment)
         end
 
         function PlotDobot(self)
-            scale = 0.8;    % scale the robot down
-            q = zeros(1,4); % generate a vector of joint angles         
+            scale = 0.8; % scale the robot down
+            q = deg2rad([0 80 15 90 0]); % starting joint angles
             self.model.plot(q,'workspace',self.workspace,'scale',scale) % plot the robot
-            self.model.teach;
-            self.model.delay = 0;   % set delay to 0 to speed up animation in trajectory
+            self.model.teach; % open teach functionality for testing purposes
+            self.model.delay = 0; % set delay to 0 to speed up animation in trajectory
         end
     end
 end
--- README.md
+++ README.md
@@ -1,8 +1,27 @@
 # Industrial Robotics 41013
 Lab Assignment 2
+* Weight: 35%
+* Proposal (group): Week 7 – 21:00 Sunday 10 April
+* Initial Feedback Stage (individual): Week 9 Lab Class – 14:00 Wednesday 27 April
+* Short Promotion Video (group): Week 11 – 21:00 Friday 13 May
+* Final System Demonstration (group): Week 12 Lab Class – 11:00 Wedneday 18 May
+* Final Video (group): Week 12 – 21:00 Friday 20 May
 
-# Project Title: DoBotany
+## Project Title: DoBotany
 Our group members are:
 * Jeremy Iskandar -13226853
 * Joshua Siwan -13199955
 * Nathan Rees -13225180
+
+## Requirements
+* Matlab GUI with advanced "teach" function and displaying individual joint movements and Cartesian [x,y,z] movements
+* Functional e-stop that stops operations and permits resuming once disengaged
+* Simulated environment with safety hardware
+* Safety functionality, including:
+	- reacting to e-stop (and resuming once hazard is cleared)
+	- preventing collisions with a simulated object is placed in system path (and resuming once hazard is cleared)
+	- retreating from simulated safety symbol using visual servoing and RMRC
+
+## Instructions
+* All functions and classes should be executed in `main_script`
+* Comments should be written throughout to explain code
